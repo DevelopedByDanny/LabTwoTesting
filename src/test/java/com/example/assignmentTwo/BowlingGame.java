e5baab313680d003e36f98d9b473546e6bdd7fe3
DannyRyd <danielrydenberg@gmail.com>
2024-02-06 13:05:29 +0100

    add roll and score methods

--- src/test/java/com/example/assignmentTwo/BowlingGame.java
+++ src/test/java/com/example/assignmentTwo/BowlingGame.java
@@ -1,4 +1,11 @@
 package com.example.assignmentTwo;
 
 public class BowlingGame {
+    public void roll(int i) {
+
+    }
+
+    public int score() {
+        return 0;
+    }
 }
--- src/test/java/com/example/assignmentTwo/BowlingTest.java
+++ src/test/java/com/example/assignmentTwo/BowlingTest.java
@@ -8,13 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingTest {
 
+    private BowlingGame game = new BowlingGame();
+
     @Test
     @DisplayName("Can we create a game")
     void canWeCreateAGame() {
-        var game = new BowlingGame();
         assertThat(game).isInstanceOf(BowlingGame.class);
     }
 
+    @Test
+    @DisplayName("Roll and miss should be zero points")
+    void rollAndMissShouldBeZeroPoints() {
+        game.roll(0);
 
+        assertThat(game.score()).isZero();
+    }
 
 }
